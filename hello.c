8d4d7421729609de3db903d5663c110a714d0f48
Grace Flitsch <graceflitsch@graces-air-2.lan>
2021-01-26 15:40:48 -0800

    Changed to say "Hello Grace" instead of "Hello World"

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 
 int main(void) {
 	// The "\n" means "start a new line"
-	printf("Hello, world!\n");
+	printf("Hello, Grace!\n");
 
 	for (int i = 10; i > 0; i--) {
 		// "%d" means "insert a decimal (base-10) int here"
